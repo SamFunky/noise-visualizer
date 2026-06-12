2603190fd50f0e7095d73b5d0b3c433b716815ff
SamFunky <115422578+SamFunky@users.noreply.github.com>
2026-06-12 17:49:19 -0600

    Create a grid of dots

--- src/components/NoiseGrid/NoiseGrid.tsx
+++ src/components/NoiseGrid/NoiseGrid.tsx
@@ -1,7 +1,32 @@
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 export function GridCanvas() {
+    const radius = .05
+    const quality = 8
+    const rows = 40
+    const cols = 40
+    const spacing = .3
+    const gridElements = []
+    const totalWidth = (cols - 1) * spacing
+    const totalHeight = (cols - 1) * spacing
+
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            const posX = (x * spacing) - (totalWidth/2)
+            const posY = (y * spacing) - (totalHeight/2)
+            const posZ = 0
+
+            gridElements.push(
+                <mesh key={'${x}-${y}'} position={[posX, posY, posZ]}>
+                    <sphereGeometry args={[radius, quality, quality]} />
+                    <meshStandardMaterial emissive={'#B8F1FF'} emissiveIntensity={1}/>
+                </mesh>
+            )
+        }
+    }
+
     return (
         <div id="canvas-container" style={{
             top: 0,
@@ -10,12 +35,9 @@ export function GridCanvas() {
             height: '100vh'
         }}>
             <Canvas>
-                <ambientLight intensity={0.1} />
-                <directionalLight color="red" position={[0, 0, 5]} />
-                <mesh>
-                    <boxGeometry args={[2, 2, 2]} />
-                    <meshStandardMaterial />
-                </mesh>
+                <OrbitControls />
+                <ambientLight intensity={0.3} />
+                {gridElements}
             </Canvas>
         </div>
     )

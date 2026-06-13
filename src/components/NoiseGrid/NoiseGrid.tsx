e94cc17e83cd2253ee48351aa132a0e1e4726fea
SamFunky <115422578+SamFunky@users.noreply.github.com>
2026-06-12 20:29:33 -0600

    Create a circle grid and taper off size as points get further from the center

--- src/components/NoiseGrid/NoiseGrid.tsx
+++ src/components/NoiseGrid/NoiseGrid.tsx
@@ -3,20 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import FastNoiseLite from 'fastnoise-lite'
-import { lerp } from 'three/src/math/MathUtils.js'
+import { lerp, MathUtils } from 'three/src/math/MathUtils.js'
 
 export function GridCanvas() {
-    const radius = .05
-    const quality = 8
-    const rows = 40
-    const cols = 40
+    const radius = .1
+    const quality = 4
+    const rows = 60
+    const cols = 60
     const spacing = .3
+    const maskRadius = (rows * spacing) / 2
     const gridElements = []
     const totalWidth = (cols - 1) * spacing
     const totalHeight = (cols - 1) * spacing
 
     const noiseFrequency = .05
-    const noiseIntensity = 3.0
+    const noiseIntensity = 1.0
     const noiseType = FastNoiseLite.NoiseType.OpenSimplex2
     const noise = useMemo(() => {
         const finalNoise = new FastNoiseLite()
@@ -44,12 +45,24 @@ export function GridCanvas() {
             const g = Math.round(lerp(g1, g2, pointNoiseValue))
             const b = Math.round(lerp(b1, b2, pointNoiseValue))
 
-            gridElements.push(
-                <mesh key={'${x}-${y}'} position={[posX, posY, posZ]}>
-                    <sphereGeometry args={[lerp(radius/4, radius, pointNoiseValue), Math.round(quality * pointNoiseValue), Math.round(quality * pointNoiseValue)]} />
-                    <meshStandardMaterial emissive={`rgb(${r}, ${g}, ${b})`} emissiveIntensity={1}/>
-                </mesh>
-            )
+            const distanceSquared = Math.pow(posX, 2) + Math.pow(posY, 2)
+            const currentDistance = Math.sqrt(distanceSquared)
+            const distanceRatio = currentDistance / maskRadius
+            const edgeScaleFactor = Math.max(0, Math.min(1, 1-distanceRatio))
+            const dotNoiseSize = lerp(radius/2, radius, pointNoiseValue)
+            const dotSize = lerp(0, dotNoiseSize, edgeScaleFactor)
+            const dotQuality = Math.round(quality * pointNoiseValue)
+
+            if (Math.pow(posX, 2) + Math.pow(posY, 2) < Math.pow(maskRadius, 2)) {
+                console.log("made it through")
+                gridElements.push(
+                    <mesh key={`${x}-${y}`} position={[posX, posY, posZ]}>
+                        <sphereGeometry args={[dotSize, dotQuality, dotQuality]} />
+                        <meshStandardMaterial emissive={`rgb(${r}, ${g}, ${b})`} emissiveIntensity={1}/>
+                    </mesh>
+                )
+            }
+            
         }
     }
 

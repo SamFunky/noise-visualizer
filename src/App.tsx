13753fffb6ab278de7127621d10b0b0093499c43
SamFunky <115422578+SamFunky@users.noreply.github.com>
2026-06-14 04:37:30 -0600

    Add basic frequency, noise type, and intensity controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css'
 import { GridCanvas } from './components/NoiseGrid/NoiseGrid'
+import { NoiseControlPannel } from './components/NoiseControls/NoiseControls'
 import FastNoiseLite from 'fastnoise-lite'
+import { useState } from 'react'
+
+type NoiseType = typeof FastNoiseLite.NoiseType[keyof typeof FastNoiseLite.NoiseType]
 
 function App() {
+  const [frequency, setFrequency] = useState(0.05)
+  const [intensity, setIntensity] = useState(1.0)
+  const [noiseType, setNoiseType] = useState<NoiseType>(FastNoiseLite.NoiseType.OpenSimplex2)
 
   return (
     <div>
-      <GridCanvas frequency={.05} type={FastNoiseLite.NoiseType.OpenSimplex2} intensity={1.0}/>
+      <NoiseControlPannel 
+        frequency={frequency}
+        setFrequency={setFrequency}
+        intensity={intensity}
+        setIntensity={setIntensity}
+        noiseType={noiseType}
+        setNoiseType={setNoiseType}
+      />
+      <GridCanvas 
+        frequency={frequency} 
+        type={noiseType} 
+        intensity={intensity}
+      />
+      
     </div>
   )
 }
--- src/components/NoiseControls/NoiseControls.tsx
+++ src/components/NoiseControls/NoiseControls.tsx
@@ -1,7 +1,87 @@
-export function NosieControls() {
+import './NoiseControls.css'
+import Slider from '@mui/material/Slider';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import type { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FastNoiseLite from 'fastnoise-lite'
+
+type NoiseType = typeof FastNoiseLite.NoiseType[keyof typeof FastNoiseLite.NoiseType]
+
+type NoiseControlProps = {
+    frequency: number
+    setFrequency: (value: number) => void
+    intensity: number
+    setIntensity: (value: number) => void
+    noiseType: NoiseType
+    setNoiseType: (value: NoiseType) => void
+}
+
+export function NoiseControlPannel({
+    frequency,
+    setFrequency,
+    intensity,
+    setIntensity,
+    noiseType,
+    setNoiseType
+
+}: NoiseControlProps) {
 
     return (
-        <div>
+        <div className='noise-controls'>
+            <Slider 
+                aria-label='frequency'
+                value={frequency}
+                size="small"
+                min={0.001}
+                max={1.0}
+                step={0.001}
+                valueLabelDisplay='auto'
+                onChange={(_, value) => setFrequency(value as number)}
+            />
+            <Slider 
+                aria-label='intensity'
+                value={intensity}
+                size="small"
+                min={0.01}
+                max={4.0}
+                step={0.01}
+                valueLabelDisplay='auto'
+                onChange={(_, value) => setIntensity(value as number)}
+            />
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                    value={noiseType}
+                    displayEmpty
+                    onChange={(event: SelectChangeEvent<NoiseType>) => setNoiseType(event.target.value as NoiseType)}
+                    inputProps={{ 'aria-label': 'Noise Type' }}
+                    sx={{
+                        color: "white",
+
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "white"
+                        },
+
+                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "white",
+                        },
+
+                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "white",
+                        },
+
+                        "& .MuiSvgIcon-root": {
+                            color: "white",
+                        },
+                    }}
+                >
+                    <MenuItem value={FastNoiseLite.NoiseType.OpenSimplex2}><em>Simplex</em></MenuItem>
+                    <MenuItem value={FastNoiseLite.NoiseType.Cellular}>Cellular</MenuItem>
+                    <MenuItem value={FastNoiseLite.NoiseType.Perlin}>Perlin</MenuItem>
+                    <MenuItem value={FastNoiseLite.NoiseType.ValueCubic}>ValueCubic</MenuItem>
+                    <MenuItem value={FastNoiseLite.NoiseType.Value}>Value</MenuItem>
+                </Select>
+            </FormControl>
         </div>
     )
 }
